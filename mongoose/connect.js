b05d9782dabff5a5cc680f9854da508f139138ca
lekubaba <lekubaba@users.noreply.github.com>
2018-09-24 18:39:43 +0800

    fd
    fd

--- mongoose/connect.js
+++ mongoose/connect.js
@@ -1,8 +1,10 @@
 var mongoose = require('mongoose');
 var logger = require('../utils/logger.js').logger;
 
-var DB_URL   = 'mongodb://wxf4754d57d42fee70:7ee0d9ca73ec7fef8ff38858b4bf3c2b@www.xiaohongxian.com:27906/feidai';
-// var DB_URL   =  'mongodb://localhost/feidai';
+
+
+// var DB_URL   = 'mongodb://wxf4754d57d42fee70:7ee0d9ca73ec7fef8ff38858b4bf3c2b@www.xiaohongxian.com:27906/feidai';
+var DB_URL   =  'mongodb://feidai:yjx123456@feidai.com.ssdsedssddsfuldsdehonkodklsjefuli76576jdjd87yuh.xiaohongxian.com:27906/feidai';
 
 mongoose.Promise = require('bluebird');
 
